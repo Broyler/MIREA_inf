705d8bf876d8f782b09eb99fbe417a49ce3ec31f
Broyler <69675212+Broyler@users.noreply.github.com>
2023-11-22 01:09:56 +0300

    Remove turtle.Turtle usage

--- 12_2.py
+++ 12_2.py
@@ -1,4 +1,4 @@
-import turtle
+from turtle import *
 from math import sqrt, asin, degrees
 from enum import Enum
 
@@ -18,13 +18,12 @@ EQUALITERAL_ANGLE = 120
 STRAIGHT_ANGLE = 180
 FINISH_ACTION = len(Shapes) + 1
 
+width(LINE_WIDTH)
+speed(DRAW_SPEED)
 
-t = turtle.Turtle()
-t.width(LINE_WIDTH)
-sc = turtle.getscreen()
-w, h = sc.window_width(), sc.window_height()
-print(w, h)
-t.speed(DRAW_SPEED)
+screen = getscreen()
+w = screen.window_width()
+h = screen.window_height()
 
 def coords(x, y):
     x = x - (w/2)
@@ -34,28 +33,28 @@ def coords(x, y):
 def line(x1, y1, x2, y2):
     x1, y1 = coords(x1, y1)
     x2, y2 = coords(x2, y2)
-    t.penup()
-    t.goto(x1, y1)
-    t.pendown()
-    t.goto(x2, y2)
-    t.penup()
+    penup()
+    goto(x1, y1)
+    pendown()
+    goto(x2, y2)
+    penup()
 
 def rect(x1,y1,x2,y2):
-    t.begin_fill()
+    begin_fill()
     line(x1,y1,x1,y2)
     line(x1,y2,x2,y2)
     line(x2,y2,x2,y1)
     line(x2,y1,x1,y1)
-    t.end_fill()
+    end_fill()
 
 def circ(x1,y1,R):
     x1, y1 = coords(x1, y1-R)
-    t.penup()
-    t.goto(x1,y1)
-    t.begin_fill()
-    t.pendown()
-    t.circle(R)
-    t.end_fill()
+    penup()
+    goto(x1,y1)
+    begin_fill()
+    pendown()
+    circle(R)
+    end_fill()
 
 def tri(x1,y1,x2,y2):
     x1, y1 = coords(x1, y1)
@@ -63,18 +62,18 @@ def tri(x1,y1,x2,y2):
     h = sqrt((x2-x1)**2 + (y2-y1)**2)
     a = h * INVERTED_SIN_60
     c = abs(y2 - y1)
-    t.penup()
-    t.goto(x1,y1)
-    t.left(degrees(asin(c/h)) + HALF_OF_ANGLE)
-    t.pendown()
-    t.begin_fill()
-    t.forward(a)
-    t.right(EQUALITERAL_ANGLE)
-    t.forward(a)
-    t.right(EQUALITERAL_ANGLE)
-    t.forward(a)
-    t.left(STRAIGHT_ANGLE + HALF_OF_ANGLE - degrees((asin(c/h))))
-    t.end_fill()
+    penup()
+    goto(x1,y1)
+    left(degrees(asin(c/h)) + HALF_OF_ANGLE)
+    pendown()
+    begin_fill()
+    forward(a)
+    right(EQUALITERAL_ANGLE)
+    forward(a)
+    right(EQUALITERAL_ANGLE)
+    forward(a)
+    left(STRAIGHT_ANGLE + HALF_OF_ANGLE - degrees((asin(c/h))))
+    end_fill()
 
 
 while True:
@@ -88,7 +87,7 @@ while True:
         except:
             print("Ошибка ввода!!!")
             continue
-        t.color("#27083d")
+        color("#27083d")
         line(x1,y1,x2,y2)
 
     elif ch == str(Shapes.RECTANGLE.value):
@@ -100,7 +99,7 @@ while True:
         except:
             print("Ошибка ввода!!!")
             continue
-        t.color("#00add7")
+        color("#00add7")
         rect(x1,y1,x2,y2)
 
     elif ch == str(Shapes.CIRCLE.value):
@@ -111,7 +110,7 @@ while True:
         except:
             print("Ошибка ввода!!!")
             continue
-        t.color("#c1300d")
+        color("#c1300d")
         circ(x1,y1,R)
 
     elif ch == str(Shapes.TRIANGLE.value):
@@ -123,7 +122,7 @@ while True:
         except:
             print("Ошибка ввода!!!")
             continue
-        t.color("#e6d500")
+        color("#e6d500")
         tri(x1,y1,x2,y2)
 
     elif ch == str(FINISH_ACTION):
@@ -133,4 +132,4 @@ while True:
         print("Ошибка!!!")
 
 
-turtle.done()
+done()
